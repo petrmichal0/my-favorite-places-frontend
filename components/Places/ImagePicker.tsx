eaea43a122cbdf1aca526f5e3d4d6f0fb77c4899
Petr Michal <petrmichal0@gmail.com>
2024-09-01 22:47:37 +0200

    Taking Photos on Android

--- components/Places/ImagePicker.tsx
+++ components/Places/ImagePicker.tsx
@@ -1,5 +1,22 @@
+import { Button, View } from "react-native";
+import { launchCameraAsync } from "expo-image-picker";
+
 function ImagePicker() {
-  return <div>ImagePicker</div>;
+  async function takeImageHandler() {
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take Image" onPress={takeImageHandler} />
+    </View>
+  );
 }
 
 export default ImagePicker;
