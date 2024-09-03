985c63425f274287b39fcb40d5d0bcf9402233a6
Petr Michal <petrmichal0@gmail.com>
2024-09-03 22:03:53 +0200

    Allowing Map Interaction & Adding Markers

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -1,17 +1,48 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
-const region = {
-  latitude: 50.0755,
-  longitude: 14.4378,
-  latitudeDelta: 0.0922, // how much content can be seen vertically
-  longitudeDelta: 0.0421, // how much content can be seen horizontally
-};
-
 function Map() {
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
-}
+  const [selectedLocation, setSelectedLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
 
+  const region = {
+    latitude: 50.0755,
+    longitude: 14.4378,
+    latitudeDelta: 0.0922, // how much content can be seen vertically
+    longitudeDelta: 0.0421, // how much content can be seen horizontally
+  };
+
+  function selectLocationHandler(event: {
+    nativeEvent: { coordinate: { latitude: number; longitude: number } };
+  }) {
+    console.log(event);
+    const { latitude, longitude } = event.nativeEvent.coordinate;
+
+    setSelectedLocation({
+      latitude,
+      longitude,
+    });
+  }
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          coordinate={selectedLocation}
+          title="Your location"
+          description="You are here"
+        />
+      )}
+    </MapView>
+  );
+}
 export default Map;
 
 const styles = StyleSheet.create({
