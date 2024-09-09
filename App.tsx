9630ccb39fb123e6188bfa6c08fe0c9b6235cd49
Petr Michal <petrmichal0@gmail.com>
2024-09-09 22:00:49 +0200

    SQLite: Getting Started & Initialization

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { ActivityIndicator } from "react-native";
 
 import { Colors } from "./constants/colors";
+
+import { init } from "./util/database";
 
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
@@ -13,6 +17,31 @@ import IconButton from "./components/Places/ui/IconButton";
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    const initializeDatabase = async () => {
+      try {
+        await init(); // Počkej na inicializaci databáze
+        setDbInitialized(true); // Nastav dbInitialized na true po úspěšné inicializaci
+      } catch (error) {
+        console.error("Failed to initialize database:", error); // Zpracování chyby
+      }
+    };
+
+    initializeDatabase();
+  }, []);
+
+  if (!dbInitialized) {
+    return (
+      <ActivityIndicator
+        size="large"
+        color={Colors.primary500}
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      />
+    );
+  }
+
   return (
     <>
       <StatusBar style="light" />
